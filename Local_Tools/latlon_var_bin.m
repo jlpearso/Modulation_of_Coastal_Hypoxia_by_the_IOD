3bbdbf94576dbc519f75e725ee6509057241a6b8
Jenna Pearson <jennalynnpearson@gmail.com>
2020-09-17 09:47:02 -0400

    Update latlon_var_bin.m

--- Local_Tools/latlon_var_bin.m
+++ Local_Tools/latlon_var_bin.m
@@ -45,7 +45,7 @@ var_vec = var(:);
 
 % use a max of 5 cores in the parfor loop
 for xx = 1:length(lonbins)
-    parfor yy = 1:length(latbins,5)
+    parfor yy = 1:length(latbins,4)
         ind = lat_disc == yy & lon_disc == xx;
         if ~isempty(lat_vec(ind))
             var_binned{yy,xx} = var_vec(ind);
